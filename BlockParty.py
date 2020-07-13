e1f256f04875680304a48c05e9000cb4488e4cce
James Cameron <quozl@laptop.org>
2020-07-13 13:36:46 +1000

    Remove unused imports

--- BlockParty.py
+++ BlockParty.py
@@ -30,9 +30,7 @@ import time
 import sys
 import random
 import copy
-import socket
 import os
-import logging
 
 import gi
 gi.require_version('Gtk', '3.0')
@@ -42,7 +40,6 @@ gi.require_version('Gst', '1.0')
 from gi.repository import Gtk
 from gi.repository import Gdk
 from gi.repository import GLib
-from gi.repository import Gst
 from gi.repository import Pango
 from gi.repository import PangoCairo
 
